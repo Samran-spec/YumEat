f06df443ec4723d9c0ea3732a69b70522358a5eb
Samran Haider <s4haider@uwaterloo.ca>
2023-10-17 21:34:26 -0400

    nav bar

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import {AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag} from 'react-icons/ai'
+import {BsFillCartFill, BsPerson} from 'react-icons/bs'
 const TopNav = () =>{
     return(
-        <div className='max-w-[1520px] mx-auto flex justify-between items-center p-4'>
+        <div className='max-w-[1520px]  mx-auto flex justify-between items-center p-4'>
             <div className='flex items-center'>
                 <div className='cursor-pointer'>
                     <AiOutlineMenu size={25}/>
@@ -14,10 +15,18 @@ const TopNav = () =>{
                     <p className='bg-orange-700 text-white rounded-full p-2 text-bold'>Free</p>
                     <p className='p-2 text-bold'>Delivery</p>
                 </div>
-                <div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]'>
+                <div className='bg-black rounded-full flex items-center px-2 py-1  w-[200px] sm:w-[400px] lg:w-[500px]'>
                     <AiOutlineSearch size={25}/>
-                    <input className='bg-transparent p-2 w-full focus:outline-none'></input>
+                    <input className='bg-transparent p-2 w-full focus:outline-none'
+                    type='text'
+                    placeholder='Search Meal'
+                    />
                 </div>
+                {/* <div>
+                    <button className='bg-orange-700 text-white hidden md:flex items-center py-2 rounded-full'> 
+                    <BsFillCartFill size={20}/>
+                    Cart</button>
+                </div> */}
             </div>
         </div>
     )
